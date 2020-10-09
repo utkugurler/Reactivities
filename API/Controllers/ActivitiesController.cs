9be28f3bfa65064375a4129236c5b566c1d4ec1e
utku gurler <utku.gurler@hotmail.com>
2020-10-09 14:31:46 +0300

    End of Section 4

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,53 @@
+using MediatR;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using Domain;
+using System.Threading.Tasks;
+using Application.Activities;
+
+namespace API.Controllers
+{
+    [Microsoft.AspNetCore.Mvc.Route("api/[controller]")]
+    [ApiController]
+    public class ActivitiesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public ActivitiesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> List()
+        {
+            return await _mediator.Send(new List.Query());  
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> Details(Guid id)
+        {
+            return await _mediator.Send(new Details.Query{Id = id});
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Unit>> Create(Create.Command command)
+        {
+            return await _mediator.Send(command);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Unit>> Edit(Guid id ,Edit.Command command)
+        {
+            command.Id = id;
+            return await _mediator.Send(command);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Unit>> Delete(Guid id)
+        {
+            return await _mediator.Send(new Delete.Command{Id = id});
+        }
+    }
+}
---
+++ Application/Activities/List.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using MediatR;
+using Domain;
+using System.Threading;
+using System.Threading.Tasks;
+using Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Activities
+{
+    public class List
+    {
+        public class Query : IRequest<List<Activity>> {}
+
+        public class Handler : IRequestHandler<Query, List<Activity>>
+        {
+            private readonly DataContext _context;
+
+            public Handler(DataContext context)
+            {
+                _context = context;
+            }
+
+
+            public async Task<List<Activity>> Handle(Query request, 
+                    CancellationToken cancellationToken)
+            {
+                var activities = await _context.Activities.ToListAsync();
+
+                return activities;
+            }
+        }
+    }
+}
